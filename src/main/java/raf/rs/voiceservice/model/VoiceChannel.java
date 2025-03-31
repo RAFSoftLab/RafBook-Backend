1433d462ea54d9bc3e9167ee6e4b3024a923366b
Petar_Stamenic_RN_77_21 <pstamenic7721rn@raf.rs>
2025-03-31 23:16:16 +0200

    UUID

--- src/main/java/raf/rs/voiceservice/model/VoiceChannel.java
+++ src/main/java/raf/rs/voiceservice/model/VoiceChannel.java
@@ -13,7 +13,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class VoiceChannel {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private Long id;
     private String name;
     private String description;
